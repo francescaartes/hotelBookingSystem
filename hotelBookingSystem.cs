8bf5a74ab4444090c80ee3a906d062589abfd9b6
Francesca Artes <122511777+francescaartes@users.noreply.github.com>
2023-05-13 14:58:53 +0800

    baseProgram

--- hotelBookingSystem.cs
+++ hotelBookingSystem.cs
@@ -1,46 +1,44 @@
-﻿Console.WriteLine("Welcome to Hotel Booking System!\n" );
+﻿Console.WriteLine("Welcome to Hotel Booking System!" );
 
-Console.WriteLine("Menu: ");
-Console.WriteLine("1. Book a hotel room");
-Console.WriteLine("2. View hotel rooms");
-Console.WriteLine("3. Exit");
+bool hotelBookingSystem = true;
+while(hotelBookingSystem) {
+    Console.WriteLine();
+    Console.WriteLine("Menu: ");
+    Console.WriteLine("1. Book a hotel room");
+    Console.WriteLine("2. View hotel rooms");
+    Console.WriteLine("3. Exit");
 
-System.Console.Write("Enter the number (1-3): ");
-string? selectedMenu = Console.ReadLine();
-
-switch(selectedMenu) {
-    case "1":
-        System.Console.WriteLine("\nAvailable Hotel Rooms: ");
-
-        for (int i = 100; i <= 110; i++) System.Console.Write(" " + i + " ");
-
-        System.Console.Write("\nEnter the room number: ");
-        string? roomNumber = Console.ReadLine();
-
-        System.Console.WriteLine("\nYou have successfully booked Room " + roomNumber + "!");
-
-    break;
+    System.Console.Write("Enter the number (1-3): ");
+    string? selectedMenu = Console.ReadLine();
     
-    case "2":
-        System.Console.WriteLine("\nHotel Rooms: ");
+    switch(selectedMenu) {
+        case "1":
+            System.Console.WriteLine("\nHotel Rooms: ");
 
-        for (int i = 100; i <= 110; i++) System.Console.Write(" " + i + " ");
+            for (int i = 100; i <= 110; i++) System.Console.Write(" " + i + " ");
 
-    break;
+            System.Console.Write("\nEnter the room number: ");
+            string? roomNumber = Console.ReadLine();
 
-    case "3":
-        System.Console.WriteLine("\nThank you for using Hotel Booking System!");
-    break;
+            System.Console.Write("Enter the date (MM/DD/YYYY): ");
+            string? dateBooked = Console.ReadLine();
+            
+            System.Console.WriteLine("\nYou have successfully booked Room " + roomNumber + " on " + dateBooked);
+        break;
+    
+        case "2":
+            System.Console.WriteLine("\nHotel Rooms: ");
 
-    default:
-        System.Console.WriteLine("\n Invalid number! Please try again.\n");
+            for (int i = 100; i <= 110; i++) System.Console.Write(" " + i + " ");
+        break;
 
-        Console.WriteLine("Menu: ");
-        Console.WriteLine("1. Book a hotel room");
-        Console.WriteLine("2. View available rooms");
-        Console.WriteLine("3. Exit");
+        case "3":
+            System.Console.WriteLine("\nThank you for using Hotel Booking System!");
+            hotelBookingSystem = false;
+        break;
 
-        System.Console.Write("Enter the number (1-3): ");
-        selectedMenu = Console.ReadLine();
-    break;
+        default:
+            System.Console.WriteLine("\n Invalid number! Please try again.");
+        break;
+    }
 }
