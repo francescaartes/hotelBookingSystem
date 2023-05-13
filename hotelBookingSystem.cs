e68b9e024ac388fadd924694136aa6d78899aadc
Francesca Artes <122511777+francescaartes@users.noreply.github.com>
2023-05-13 18:07:18 +0800

    Selecting Room Enhanced
    Removed choosing room number and added room types and their number of rooms available.

--- hotelBookingSystem.cs
+++ hotelBookingSystem.cs
@@ -1,6 +1,11 @@
 ﻿Console.WriteLine("Welcome to Hotel Booking System!" );
 
 bool hotelBookingSystem = true;
+
+    int singleRooms = 15;
+    int doubleRooms = 10;
+    int familyRooms = 5; 
+
 while(hotelBookingSystem) {
     Console.WriteLine();
     Console.WriteLine("Menu: ");
@@ -10,26 +15,45 @@ while(hotelBookingSystem) {
 
     System.Console.Write("Enter the number (1-3): ");
     string? selectedMenu = Console.ReadLine();
-    
+
     switch(selectedMenu) {
         case "1":
             System.Console.WriteLine("\nHotel Rooms: ");
 
-            for (int i = 100; i <= 110; i++) System.Console.Write(" " + i + " ");
+            System.Console.WriteLine("\tType of Rooms:      No. of Available rooms:");
+            System.Console.WriteLine("\t1. Single           " + singleRooms);
+            System.Console.WriteLine("\t2. Double           " + doubleRooms);
+            System.Console.WriteLine("\t3. Family           " + familyRooms);
 
-            System.Console.Write("\nEnter the room number: ");
-            string? roomNumber = Console.ReadLine();
-
+            System.Console.Write("\nEnter the type of room (1-3): ");
+            string? roomType = Console.ReadLine();
+            
             System.Console.Write("Enter the date (MM/DD/YYYY): ");
             string? dateBooked = Console.ReadLine();
-            
-            System.Console.WriteLine("\nYou have successfully booked Room " + roomNumber + " on " + dateBooked);
+
+            if (roomType == "1") {
+                singleRooms = singleRooms - 1;
+                roomType = "Single Room";
+                }
+            else if (roomType == "2") {
+                doubleRooms = doubleRooms - 1;
+                roomType = "Double Room";
+                }
+            else if(roomType == "3") {
+                familyRooms = familyRooms - 1;
+                roomType = "Family Room";
+                }
+
+            System.Console.WriteLine("\nYou have successfully booked a " + roomType + " on " + dateBooked);
         break;
     
         case "2":
             System.Console.WriteLine("\nHotel Rooms: ");
 
-            for (int i = 100; i <= 110; i++) System.Console.Write(" " + i + " ");
+            System.Console.WriteLine("\tType of Rooms:      No. of Available rooms:");
+            System.Console.WriteLine("\t1. Single           " + singleRooms);
+            System.Console.WriteLine("\t2. Double           " + doubleRooms);
+            System.Console.WriteLine("\t3. Family           " + familyRooms);
         break;
 
         case "3":
